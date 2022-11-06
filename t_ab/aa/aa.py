741009b07421f7fcff6f472b07e69cd78dd8a40d
Keisuke Hirota <tahiro.k.ad@gmail.com>
2022-11-06 18:47:02 +0900

    Refactor

--- t_ab/aa/aa.py
+++ t_ab/aa/aa.py
@@ -17,23 +17,26 @@ class AATest:
         method: str = "ks",
         alpha: float = 0.05,
     ) -> None:
+        assert method in {"ks"}
         self.data_loader = data_loader
         self.n_tests = n_tests
         self.method = method
         self.alpha = alpha
 
-    def __call__(
-        self,
-        *test_funcs: Callable[[list], float],
-    ) -> list[AATestResult]:
-        pvalues: list[list[float]] = [[] for _ in test_funcs]
+    def __call__(self, *test_funcs: Callable[[list], float]) -> list[AATestResult]:
+        pvalues_list = self.run_tests(*test_funcs)
+        results = [self.test_pvalues(pvalues) for pvalues in pvalues_list]
+        return results
+
+    def run_tests(self, *test_funcs: Callable[[list], float]) -> list[list[float]]:
+        pvalues_list: list[list[float]] = [[] for _ in test_funcs]
         for i in range(self.n_tests):
             data = self.data_loader(i)
             for j, test_func in enumerate(test_funcs):
-                pvalues[j].append(test_func(data))
+                pvalues_list[j].append(test_func(data))
+        return pvalues_list
 
-        results = []
-        for pvs in pvalues:
-            pvalue = kstest(pvs, "uniform").pvalue
-            results.append(AATestResult(pvs, pvalue, pvalue < self.alpha))
-        return results
+    def test_pvalues(self, pvalues: list[float]) -> AATestResult:
+        if self.method == "ks":
+            pvalue = kstest(pvalues, "uniform").pvalue
+        return AATestResult(pvalues, pvalue, pvalue < self.alpha)
