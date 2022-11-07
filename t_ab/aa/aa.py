e2734350de6959a84eaac2c71fffe5322e7e092b
Keisuke Hirota <keisuke.hirota@bizreach.co.jp>
2022-11-07 16:40:09 +0900

    Refactor AA testing

--- t_ab/aa/aa.py
+++ t_ab/aa/aa.py
@@ -1,11 +1,14 @@
 from typing import Callable, NamedTuple
 
+import numpy as np
 from scipy.stats import kstest
+
+from t_ab.multiple_comparison import bonferroni
 
 
 class AATestResult(NamedTuple):
+    pvalues_arr: np.ndarray
     pvalues: list[float]
-    pvalue: float
     is_rejected: bool
 
 
@@ -14,29 +17,47 @@ class AATest:
         self,
         data_loader: Callable[[int], list],
         n_tests: int = 1000,
-        method: str = "ks",
         alpha: float = 0.05,
+        uniform_test_method: str = "ks",
+        mcp_correction_method: str = "bonferroni",
     ) -> None:
-        assert method in {"ks"}
+        assert uniform_test_method in {"ks"}
+        assert mcp_correction_method in {"bonferroni"}
         self.data_loader = data_loader
         self.n_tests = n_tests
-        self.method = method
         self.alpha = alpha
+        self.uniform_test_method = uniform_test_method
+        self.mcp_correction_method = mcp_correction_method
 
-    def __call__(self, *test_funcs: Callable[[list], float]) -> list[AATestResult]:
+    @property
+    def uniform_test(self) -> Callable:
+        if self.uniform_test_method == "ks":
+            return kstest
+        raise ValueError
+
+    @property
+    def mcp_correction(self) -> Callable:
+        if self.mcp_correction_method == "bonferroni":
+            return bonferroni
+        raise ValueError
+
+    def __call__(
+        self, *test_funcs: Callable[[list], list[float]]
+    ) -> list[AATestResult]:
         pvalues_list = self.run_tests(*test_funcs)
         results = [self.test_pvalues(pvalues) for pvalues in pvalues_list]
         return results
 
-    def run_tests(self, *test_funcs: Callable[[list], float]) -> list[list[float]]:
-        pvalues_list: list[list[float]] = [[] for _ in test_funcs]
+    def run_tests(self, *test_funcs: Callable[[list], list[float]]) -> np.ndarray:
+        pvalues_list: list[list[list[float]]] = [[] for _ in test_funcs]
         for i in range(self.n_tests):
             data = self.data_loader(i)
             for j, test_func in enumerate(test_funcs):
                 pvalues_list[j].append(test_func(data))
-        return pvalues_list
+        return np.array(pvalues_list).transpose(0, 2, 1)
 
-    def test_pvalues(self, pvalues: list[float]) -> AATestResult:
-        if self.method == "ks":
-            pvalue = kstest(pvalues, "uniform").pvalue
-        return AATestResult(pvalues, pvalue, pvalue < self.alpha)
+    def test_pvalues(self, pvalues_arr: np.ndarray) -> AATestResult:
+        pvalues = [kstest(pvalues, "uniform").pvalue for pvalues in pvalues_arr]
+        return AATestResult(
+            pvalues_arr, pvalues, self.mcp_correction(pvalues, self.alpha)
+        )
