566fd28b3aaffa5561930f8bd71f504cca266caf
Keisuke Hirota <keisuke.hirota@bizreach.co.jp>
2022-11-07 17:24:51 +0900

    Refactor

--- t_ab/aa/aa.py
+++ t_ab/aa/aa.py
@@ -1,4 +1,4 @@
-from typing import Callable, NamedTuple
+from typing import Callable, Generator, NamedTuple
 
 import numpy as np
 from scipy.stats import kstest
@@ -6,7 +6,7 @@ from statsmodels.stats.multitest import multipletests
 
 
 class AATestResult(NamedTuple):
-    pvalues_for_test: np.ndarray
+    pvalues_for_aa_test: list[list[float]]
     multipletests_result: tuple[np.ndarray, np.ndarray, float, float]
 
     @property
@@ -30,30 +30,34 @@ class AATest:
         self.uniform_test_method = uniform_test_method
         self.mcp_correction_method = mcp_correction_method
 
-    @property
-    def uniform_test(self) -> Callable:
-        if self.uniform_test_method == "ks":
-            return kstest
-        raise ValueError
-
     def __call__(
         self, *test_funcs: Callable[[list], list[float]]
     ) -> list[AATestResult]:
-        pvalues_for_test = self.run_tests(*test_funcs)
-        results = [self.test_pvalues(pvalues) for pvalues in pvalues_for_test]
-        return results
+        return [
+            self.test_pvalues(pvalues_for_aa_test)
+            for pvalues_for_aa_test in self.run_tests(*test_funcs)
+        ]
 
-    def run_tests(self, *test_funcs: Callable[[list], list[float]]) -> np.ndarray:
+    def run_tests(
+        self, *test_funcs: Callable[[list], list[float]]
+    ) -> Generator[list[list[float]], None, None]:
         pvalues_list: list[list[list[float]]] = [[] for _ in test_funcs]
         for i in range(self.n_tests):
             data = self.data_loader(i)
             for j, test_func in enumerate(test_funcs):
                 pvalues_list[j].append(test_func(data))
-        return np.array(pvalues_list).transpose(0, 2, 1)
 
-    def test_pvalues(self, pvalues_for_test: np.ndarray) -> AATestResult:
-        pvalues = [kstest(pvalues, "uniform").pvalue for pvalues in pvalues_for_test]
+        for pvalues in pvalues_list:
+            yield np.array(pvalues).T.tolist()
+
+    def test_pvalues(self, pvalues_for_aa_test: list[list[float]]) -> AATestResult:
+        pvalues = [self.uniform_test(pvalues) for pvalues in pvalues_for_aa_test]
         return AATestResult(
-            pvalues_for_test,
+            pvalues_for_aa_test,
             multipletests(pvalues, alpha=self.alpha, method=self.mcp_correction_method),
         )
+
+    def uniform_test(self, pvalues: list[float]) -> float:
+        if self.uniform_test_method == "ks":
+            return kstest(pvalues, "uniform").pvalue
+        raise ValueError
