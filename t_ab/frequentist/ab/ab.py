ad8e284e47c75adfc4627d806f17fb0e6de7e0cf
Keisuke Hirota <keisuke.hirota@bizreach.co.jp>
2023-04-03 19:17:27 +0900

    Refactor

--- t_ab/frequentist/ab/ab.py
+++ t_ab/frequentist/ab/ab.py
@@ -1,9 +1,8 @@
 from itertools import combinations
-from typing import Callable
 
 import pandas as pd
 
-from ..ctr.base import CTRTestResult, CTRTtestBase
+from ..ctr.base import CTRTtestBase
 
 
 class ABTest:
@@ -11,18 +10,32 @@ class ABTest:
         self.test_funcs = test_funcs
         self.alpha = alpha
 
-    def __call__(self, dfs: list[pd.DataFrame]) -> dict[str, dict[str, CTRTestResult]]:
-        return {
-            test_func.metrics_name: self.run_test(test_func, dfs)
-            for test_func in self.test_funcs
-        }
+    def __call__(self, dfs: list[pd.DataFrame]) -> pd.DataFrame:
+        return pd.concat(
+            [self.run_test(test_func, dfs) for test_func in self.test_funcs],
+            ignore_index=True,
+        )
 
     def run_test(
-        self,
-        test_func: Callable[[pd.DataFrame, pd.DataFrame], CTRTestResult],
-        dfs: list[pd.DataFrame],
-    ) -> dict[str, CTRTestResult]:
-        return {
+        self, test_func: CTRTtestBase, dfs: list[pd.DataFrame]
+    ) -> pd.DataFrame:
+        results = {
             f"({i}, {j})": test_func(df_i, df_j)
             for (i, df_i), (j, df_j) in combinations(zip(range(len(dfs)), dfs), 2)
         }
+        return pd.DataFrame(
+            [
+                {
+                    "metrics_base": test_func.METRICS_BASE,
+                    "metrics": test_func.metrics_name,
+                    "pair": pair,
+                    "mean_l": result.statistics[0].mean,
+                    "std_l": result.statistics[0].std,
+                    "mean_r": result.statistics[1].mean,
+                    "std_r": result.statistics[1].std,
+                    "pvalue": result.test_result.pvalue,
+                    "is_rejected": result.test_result.is_rejected,
+                }
+                for pair, result in results.items()
+            ]
+        )
