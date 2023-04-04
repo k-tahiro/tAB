202e5c10793ec9ce33c7f4371a4f66cb10ad4d64
Keisuke Hirota <keisuke.hirota@bizreach.co.jp>
2023-04-04 15:53:47 +0900

    Use percentile

--- t_ab/frequentist/ctr/user_based.py
+++ t_ab/frequentist/ctr/user_based.py
@@ -1,6 +1,5 @@
-from typing import Optional
-
 import pandas as pd
+from scipy.stats import beta
 
 from .base import CTRTtestBase, Statistics
 
@@ -9,26 +8,19 @@ class UserBasedCTRTtest(CTRTtestBase):
     METRICS_BASE = "User-Based"
 
     def ignore_outliers(
-        self, df: pd.DataFrame, outlier_percentile: Optional[float] = None
+        self,
+        df: pd.DataFrame,
+        outlier_percentile: float = 0.99,
+        estimate_dist: bool = True,
     ) -> pd.DataFrame:
         df_cluster = self.agg_cluster(df)
         metrics = self.calc_metrics(df_cluster)
-        if outlier_percentile is None:
-            q1 = metrics.quantile(0.25)
-            q3 = metrics.quantile(0.75)
-            iqr = q3 - q1
-            lower_threshold = q1 - 1.5 * iqr
-            upper_threshold = q3 + 1.5 * iqr
-            return df[
-                df[self.cluster_col].isin(
-                    df_cluster[
-                        (metrics >= lower_threshold) & (metrics <= upper_threshold)
-                    ].index
-                )
-            ]
+        if estimate_dist:
+            a, b, _, _ = beta.fit(metrics, floc=0, fscale=1, method="MM")
+            threshold = beta.ppf(outlier_percentile, a, b)
         else:
             threshold = metrics.quantile(outlier_percentile)
-            return df[df.index.isin(df_cluster[metrics <= threshold].index)]
+        return df[df[self.cluster_col].isin(df_cluster[metrics <= threshold].index)]
 
     def calc_metrics(self, df: pd.DataFrame) -> pd.Series:
         return df[self.numerator_col] / df[self.denominator_col]
